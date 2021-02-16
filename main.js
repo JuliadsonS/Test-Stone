b61e72e5da1b0b6e735180a48b3d144496f4e51c
JuliadsonS <juliadson.sm@gmail.com>
2021-02-16 16:47:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,23 @@ var items = require('./items')
 var clients = require('./clients')
 
 const accountList = function (listItems, listClients, typeUnit) {
+    let result = []
+    let valid = 0
+    let ultkey = ''
+
     let totalItems = listItems.reduce((acc, item) => acc += item.price[typeUnit] * item.amount, 0)
-    let result = listClients.map(item => {
-        return {
-            client: item,
-            total: parseInt((totalItems / clients.length).toFixed(0))
-        }
-    })
+    listClients.map(item => result[item] = parseInt((totalItems / clients.length).toFixed(0)))
+    result.reduce((acc, item) => acc += item, 0)
 
-    let valid = result.reduce((acc, item) => acc += item.total, 0)
+    for (const item in result) {
+        valid += result[item]
+        ultkey = item
+    }
 
-    if (valid != totalItems)
-        result[result.length - 1].total += totalItems - valid
+    if (valid < totalItems)
+        result[ultkey] += totalItems - valid
 
     return result
 }
 
-console.log("Result: ", accountList(items, clients, "pack"))
+console.log("Result: ", accountList(items, clients, "unit"))
